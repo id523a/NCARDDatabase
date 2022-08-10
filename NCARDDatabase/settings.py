bec2311c548e8ca7414d34a37798fa3a5a2c59c1
Edward Giles <edward@giles.net.au>
2022-08-10 16:28:00 +0800

    Break the build

--- NCARDDatabase/settings.py
+++ NCARDDatabase/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+Test change that breaks the build
 from pathlib import Path
 import os
 import sys
