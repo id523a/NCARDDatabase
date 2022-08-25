be59c3839ce400402721d5d534404ed54df0bf14
Edward Giles <edward@giles.net.au>
2022-08-25 21:47:44 +0800

    Add Projects to admin interface

--- ncard_app/admin.py
+++ ncard_app/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from ncard_app import models
 
 admin.site.register(models.Person)
-admin.site.register(models.PersonAddress)
 admin.site.register(models.Organisation)
 admin.site.register(models.ContactRecord)
+admin.site.register(models.PersonAddress)
+admin.site.register(models.Project)
