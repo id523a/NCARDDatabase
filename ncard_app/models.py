e969d0a4e618302593687409863b0495f3461263
NickkeyNikNik <nathanielong98@gmail.com>
2022-08-26 12:10:34 +0800

    added configuration for remaining tables still incomplete

--- ncard_app/models.py
+++ ncard_app/models.py
@@ -1,3 +1,7 @@
+from operator import truediv
+import profile
+from sunau import AUDIO_FILE_ENCODING_DOUBLE
+from unittest.loader import VALID_MODULE_NAME
 from django.conf import settings
 from django.db import models
 from django.core.validators import RegexValidator, MinValueValidator
@@ -31,13 +35,157 @@ class Person(models.Model):
         ]
 
 class Organisation(models.Model):
-    name = models.CharField(max_length=255)
+    organisation_ID = models.CharField('Organisation ID', max_length=50, blank=True)
+    organisation_name = models.CharField('Organisation name', max_length=255, blank=True)
+    primary_contact = models.CharField('Primary Contact', max_length=255, blank=True)
+    organisation_phone = models.CharField('Organisation Phone', max_length=25, blank=True)
+    website = models.CharField('Website', max_length=255, blank=True)
+    twitter_handle = models.CharField('Twitter Handle', max_length=255, blank=True)
+    organisation_type = models.CharField('Organisation Type', max_length=255, blank=True)
 
     def __str__(self):
         return self.name
         
     class Meta:
         ordering = ['name']
+
+class HumanResource(models.Model):
+    human_resource_ID = models.CharField('Human Resource ID', max_length=50, blank=True)
+    contact_ID = models.CharField('Contact ID', max_length=50, blank=True)
+    group_lead = models.CharField('Group Lead', max_length=255, blank=True)
+    line_manager = models.CharField('Line Manager', max_length=255, blank=True)
+    student_ID = models.CharField('Student ID', max_length=50, blank=True)
+    staff_ID = models.CharField('Staff ID', max_length=50, blank=True)
+    uwa_ID = models.CharField('UWA ID', max_length=50, blank=True)
+    school_centre = models.CharField('School Centre', max_length=255, blank=True)
+    hr_role = models.CharField('HR Role', max_length=255, blank=True)
+    hr_start_date = models.DateField('HR Start Date', blank=True)
+    hr_end_date = models.DateField('HR End Date', blank=True)
+    year_phd_awarded = models.IntegerField('Year PHD Awarded', max_length=4,blank=True)
+    employer1 = models.CharField('Employer 1', max_length=255, blank=True)
+    employer2 = models.CharField('Employer 2', max_length=255, blank=True)
+    hr_classification = models.CharField('HR Classification', max_length=255, blank=True)
+    hr_level = models.IntegerField('HR Level', blank=True)
+    fte = models.CharField('FTE', max_length=255, blank=True)
+    cake_date = models.DateField('Cake Date', blank=True)
+
+class Project(models.Model):
+    class ProjectStatus(models.IntegerChoices):
+        NONE = 0, '-'
+        PENDING = 1, 'Pending'
+        ACTIVE = 2, 'Active'
+        COMPLETE = 3, 'Complete'
+
+    name = models.CharField(max_length=255)
+    lead = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True, blank=True, related_name='projects')
+    status = models.IntegerField(choices=ProjectStatus.choices, default=ProjectStatus.NONE)
+    funded = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name'])
+        ]
+
+class grants(models.Model):
+    grant_ID = models.CharField('Grant ID', max_length=50, blank=True)
+    grant_reference = models.CharField('Grant Reference', max_length=255, blank=True)
+    grant_title = models.CharField('Grant Title', max_length=255, blank=True)
+    project_name = models.CharField('Project Name', max_length=255, blank=True)
+    grant_lead_FN = models.CharField('Grant Lead FN', max_length=255, blank=True)
+    grant_all_investigators = models.CharField('Grant All Investigators', max_length=255, blank=True)
+    funding_agency = models.CharField('Funding Agency', max_length=255, blank=True)
+    year_submitted = models.IntegerField('Year Submitted', max_length=4,blank=True)
+    total_award = models.IntegerField('Total Award', blank=True)
+    grant_status = models.CharField('Grant Status', max_length=255, blank=True)
+    year_start = models.IntegerField('Year Start', max_length=4,blank=True)
+    year_end = models.IntegerField('Year End', max_length=4,blank=True)
+    business_unit = models.CharField('Business Unit', max_length=255, blank=True)
+    uwa_pgnumber = models.IntegerField('UWA PG Number', blank=True)
+
+class engagement(models.Model):
+    engagement_ID = models.CharField('Engagement ID', max_length=50, blank=True)
+    contact_name = models.CharField('Contact Name', max_length=255, blank=True)
+    engagement_type = models.CharField('Engagement Type', max_length=255, blank=True)
+    engagement_title = models.CharField('Engagement Title', max_length=255, blank=True)
+    lead_organisation = models.CharField('Lead Organisation', max_length=255, blank=True)
+    engagement_detail = models.CharField('Engagement Detail', max_length=255, blank=True)
+    engagement_year = models.IntegerField('Engagement Year', max_length=4,blank=True)
+    engagement_outcome = models.CharField('Engagement Outcome', max_length=255, blank=True)
+    media_link = models.CharField('Media Link', max_length=255, blank=True)
+
+class awards(models.Model):
+    award_ID = models.CharField('Award ID', max_length=50, blank=True)
+    award_type = models.CharField('Award Type', max_length=255, blank=True)
+    award_agency = models.CharField('Award Agency', max_length=255, blank=True)
+    award_name = models.CharField('Award Name', max_length=255, blank=True)
+    award_recipient = models.CharField('Award Recipient', max_length=255, blank=True)
+    award_status = models.CharField('Award Status', max_length=255, blank=True)
+    award_detail =  models.TextField('Award Detail', blank=True)
+    award_year = models.IntegerField('Award Year', max_length=4,blank=True)
+
+class biography:
+    bio_ID = models.CharField('Biography ID', max_length=50, blank=True)
+    contact_ID = models.CharField('Contact ID', max_length=50, blank=True)
+    bio_type = models.CharField('Bio Type', max_length=255, blank=True)
+    profile_contact_name = models.CharField('Profile Contact Name', max_length=255, blank=True)
+
+class events:
+    event_ID = models.CharField('Event ID', max_length=50, blank=True)
+    event_type = models.CharField('Event Type', max_length=255, blank=True)
+    event_date = models.DateField('Event Date', blank=True)
+    number_attendees = models.BigIntegerField('Number Of Attendees', blank=True)
+    event_detail = models.TextField(blank=True)
+    event_lead = models.CharField('Event Lead', max_length=255, blank=True)
+    event_location = models.CharField('Event Location', blank=True)
+
+class governance:
+    field1 = models.CharField('Field1', blank=True)
+    governance_ID = models.CharField('Governance ID', max_length=50, blank=True)
+    governance_group = models.CharField('Governance Group', max_length=255,blank=True)
+    member_name = models.CharField('Member Name', max_length=255,blank=True)
+
+class enewssubscribers:
+    e_news_ID = models.CharField('eNews ID', max_length=50, blank=True)
+    first_name = models.CharField('First Name', max_length=64)
+    last_name = models.CharField('Last Name', max_length=64)
+    email_address = models.EmailField('email Address', blank=True)
+    category = models.CharField('category', blank=True)
+    audience_type = models.CharField('Audience Type', blank=True)
+
+class students:
+    student_ID = models.CharField('Student ID', max_length=50, blank=True)
+    student_name = models.CharField('Student Name', max_length=64)
+    student_type = models.CharField('Student Type', blank=True)
+    primary_supervisor = models.CharField('Primary supervisor', max_length=255,blank=True)
+    supervisor2 = models.CharField('Second supervisor', max_length=255,blank=True)
+    supervisor3 = models.CharField('Third supervisor', max_length=255,blank=True)
+    title_topic = models.TextField('title topic', blank=True)
+    year_start = models.IntegerField('Award Year', max_length=4,blank=True)
+    year_end = models.IntegerField('Award Year', max_length=4,blank=True)
+    scholarship = models.CharField('Scholarship', max_length=255,blank=True)
+
+class publications:
+    publication_ID = models.CharField('Publication ID', max_length=50, blank=True)
+    publication_type = models.CharField('Publication Type', blank=True)
+    publication_year = models.DateField('Publication Date', blank=True)
+    publication_title = models.CharField('Publication Title', max_length=255, blank=True)
+    contributors = models.CharField('contributors', blank=True)
+    journal = models.CharField('journal', max_length=255, blank=True)
+    journal_ISSN = models.CharField('Journal ISSN', blank=True)
+    volume = models.CharField('Volume', blank=True)
+    pages = models.IntegerField('Pages', blank=True)
+    open_access_status = models.CharField('open access status', blank=True)
+    doi = models.CharField('DOI', blank=True)
+    electronic_ISBN = models.CharField('Electronic ISBN', blank=True)
+    print_ISBN = models.CharField('Print ISBN', blank=True)
+    abstract =models.CharField('Abstract', blank=True)
+    vancouver =models.CharField('Vancouver', blank=True)
+    source_ID = models.CharField('source ID', max_length=50, blank=True)
+    ncard_publication = models.CharField('NCARD publication', blank=True)
 
 class ContactRecord(models.Model):
     class NCARDRelation(models.IntegerChoices):
