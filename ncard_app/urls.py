cbe6c908a9f641440815f2c46d481cbd28757a32
ZackUWA <22827028@student.uwa.edu.au>
2022-09-04 20:41:49 +0800

    delete the duplicated url

--- ncard_app/urls.py
+++ ncard_app/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
 
     path('', views.index, name='index'),
     path("test/show", views.CbShow.as_view(), name='fuzzy'),
-
-
     path('home', views.home, name="home"),
     path('login_user', views.login_user, name="login"),
     path('logout_user', views.logout_user, name="logout"),
