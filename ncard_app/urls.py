8725190ac4067bd96c681fd9d73ac8be46d186c2
ZackUWA <22827028@student.uwa.edu.au>
2022-09-04 20:41:31 +0800

    Update urls.py

--- ncard_app/urls.py
+++ ncard_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path("test/show", views.CbShow.as_view(), name='fuzzy'),
 
-    path('', views.index, name="index"),
+
     path('home', views.home, name="home"),
     path('login_user', views.login_user, name="login"),
     path('logout_user', views.logout_user, name="logout"),
