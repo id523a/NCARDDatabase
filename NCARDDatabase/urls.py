39024c47f9826a808010193834613d8c851d4646
Edward Giles <edward@giles.net.au>
2022-09-05 23:32:14 +0800

    Use crispy-forms for rendering

--- NCARDDatabase/urls.py
+++ NCARDDatabase/urls.py
@@ -20,6 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ncard_app.urls')),
-    path('ncard_app/', include('ncard_app.urls')),
 ]
  
--- ncard_app/urls.py
+++ ncard_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from ncard_app import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name="index"),
     path('home', views.home, name="home"),
-    path('login_user', views.login_user, name="login"),
-    path('logout_user', views.logout_user, name="logout"),
+    path('login_user', auth_views.LoginView.as_view(), name="login"),
+    path('logout_user', auth_views.logout_then_login, name="logout"),
     path("test/show",views.test_show,name='fuzzy'),
 ]
