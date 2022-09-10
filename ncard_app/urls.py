46b5617a9c95f5050d4a49d29c3f6d6eea62b996
Edward Giles <edward@giles.net.au>
2022-09-10 16:05:14 +0800

    Allow adding/editing of people

--- ncard_app/urls.py
+++ ncard_app/urls.py
@@ -5,10 +5,14 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('home', views.home, name="home"),
-    path('login_user', auth_views.LoginView.as_view(), name="login"),
-    path('logout_user', auth_views.logout_then_login, name="logout"),
-    path("test/show",views.test_show,name='fuzzy'),
-    path('tables/people', views.list_people, name="list-people"),
-    path('save_people', views.save_people, name="save_people"),
+    path('home/', views.home, name="home"),
+    path('login_user/', auth_views.LoginView.as_view(), name="login"),
+    path('logout_user/', auth_views.logout_then_login, name="logout"),
+    path('tables/people/', views.list_people, name="list-people"),
+    path('tables/people/add/', views.PersonDetail.as_view(), name="add-person"),
+    path('tables/people/<int:id>/', views.PersonDetail.as_view(), name="edit-person"),
+    
+    path('save_people/', views.save_people, name="save_people"),
+    
+    path("test/show/",views.test_show,name='fuzzy'),
 ]
--- ncard_app/views/__init__.py
+++ ncard_app/views/__init__.py
@@ -1,4 +1,5 @@
 from .home import *
 from .index import *
 from .people import *
+from .person_detail import *
 from .test_show import *
