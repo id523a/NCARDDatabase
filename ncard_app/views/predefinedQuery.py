17795c9f7a74b6c0cca8220c2c35a2fd7f11cd2c
ZackUWA <22827028@student.uwa.edu.au>
2022-09-11 22:29:48 +0800

    implement the export functionality (export the result to csv file)

--- ncard_app/views/predefinedQuery.py
+++ ncard_app/views/predefinedQuery.py
@@ -1,3 +1,6 @@
+import csv
+import json
+
 from django.views import View
 from ncard_app import forms, models
 from django.shortcuts import render, HttpResponse, redirect
@@ -12,3 +15,12 @@ class PhoneBook(View):
         # print(datalist)
         return render(request, self.template_name, {"data": datalist})
 
+
+def export(request):
+    datalist = json.loads(request.POST.get('datalist'))
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="organisation.csv"'
+    writer = csv.writer(response)
+    for row in datalist:
+        writer.writerow(row)
+    return response
