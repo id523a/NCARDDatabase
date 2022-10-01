331c5a756df8fd30ef93c9d82ca8ed334f551ff1
Edward Giles <edward@giles.net.au>
2022-10-01 12:34:50 +0800

    Re-enable unit tests

--- ncard_app/test_models.py
+++ ncard_app/test_models.py
@@ -25,82 +25,78 @@ class PersonTests(TestCase):
         self.assertEqual(self.curie.full_name, 'Marie Curie')
         self.assertEqual(self.chairman.full_name, 'Mao Zedong')
         self.assertEqual(self.mononym.full_name, 'Diogenes')
-"""
+
 class ContactRecordTests(TestCase):
     def setUp(self):
         self.person = models.Person(title='Mr.', given_name='Edward', surname='Giles')
         self.person.save()
-        self.contact_record = models.ContactRecord(person=self.person)
-        self.contact_record.save()
 
     def test_default(self):
         self.person.full_clean()
-        self.contact_record.full_clean()
 
     def test_email_validation(self):
-        self.contact_record.email = 'valid.email!+0123@example.com'
-        self.contact_record.email2 = 'valid.email!+0123@example.com'
-        self.contact_record.full_clean()
+        self.person.email = 'valid.email!+0123@example.com'
+        self.person.email2 = 'valid.email!+0123@example.com'
+        self.person.full_clean()
 
         with self.assertRaises(ValidationError):
-            self.contact_record.email = 'invalid.email@example'
-            self.contact_record.full_clean()
+            self.person.email = 'invalid.email@example'
+            self.person.full_clean()
 
         with self.assertRaises(ValidationError):
-            self.contact_record.email2 = 'invalid.email@example'
-            self.contact_record.full_clean()
+            self.person.email2 = 'invalid.email@example'
+            self.person.full_clean()
 
     def test_phone_validation(self):
-        self.contact_record.phone_office = '#()*+,- 0123456789'
-        self.contact_record.phone_mobile = '+61 (0) 456-789-012'
-        self.contact_record.phone_home = '11'
-        self.contact_record.full_clean()
+        self.person.phone_office = '#()*+,- 0123456789'
+        self.person.phone_mobile = '+61 (0) 456-789-012'
+        self.person.phone_home = '11'
+        self.person.full_clean()
 
         with self.assertRaises(ValidationError):
-            self.contact_record.phone_office = 'invalid'
-            self.contact_record.full_clean()
+            self.person.phone_office = 'invalid'
+            self.person.full_clean()
 
         with self.assertRaises(ValidationError):
-            self.contact_record.phone_mobile = 'invalid'
-            self.contact_record.full_clean()
+            self.person.phone_mobile = 'invalid'
+            self.person.full_clean()
 
         with self.assertRaises(ValidationError):
-            self.contact_record.phone_home = 'invalid'
-            self.contact_record.full_clean()
+            self.person.phone_home = 'invalid'
+            self.person.full_clean()
 
     def test_orcid_validation(self):
-        self.contact_record.orcid_id = 'https://orcid.org/0000-0002-1694-233X'
-        self.contact_record.full_clean()
+        self.person.orcid_id = 'https://orcid.org/0000-0002-1694-233X'
+        self.person.full_clean()
 
         with self.assertRaises(ValidationError):
-            self.contact_record.orcid_id = 'invalid'
-            self.contact_record.full_clean()
+            self.person.orcid_id = 'invalid'
+            self.person.full_clean()
 
         with self.assertRaises(ValidationError):
-            self.contact_record.orcid_id = '0000-0002-1694-233X'
-            self.contact_record.full_clean()
+            self.person.orcid_id = '0000-0002-1694-233X'
+            self.person.full_clean()
 
         with self.assertRaises(ValidationError):
-            self.contact_record.orcid_id = 'https://orcid.org/000000021694233X'
-            self.contact_record.full_clean()
+            self.person.orcid_id = 'https://orcid.org/000000021694233X'
+            self.person.full_clean()
 
         with self.assertRaises(ValidationError):
-            self.contact_record.orcid_id = 'http://orcid.org/0000-0002-1694-233X'
-            self.contact_record.full_clean()
+            self.person.orcid_id = 'http://orcid.org/0000-0002-1694-233X'
+            self.person.full_clean()
 
     def test_twitter_validation(self):
-        self.contact_record.twitter = '@xxxxxx_Yyyyyy'
-        self.contact_record.full_clean()
+        self.person.twitter = '@xxxxxx_Yyyyyy'
+        self.person.full_clean()
 
         with self.assertRaises(ValidationError):
-            self.contact_record.twitter = 'missing_at'
-            self.contact_record.full_clean()
+            self.person.twitter = 'missing_at'
+            self.person.full_clean()
 
         with self.assertRaises(ValidationError):
-            self.contact_record.twitter = '@overlongoverlong'
-            self.contact_record.full_clean()
+            self.person.twitter = '@overlongoverlong'
+            self.person.full_clean()
 
         with self.assertRaises(ValidationError):
-            self.contact_record.twitter = '@INVALID_CHAR$'
-            self.contact_record.full_clean()
-"""
+            self.person.twitter = '@INVALID_CHAR$'
+            self.person.full_clean()
