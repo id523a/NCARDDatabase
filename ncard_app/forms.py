b41d8438816f4ddb05ab26107874a8d6d23c2854
jordansylee <jordansylee@gmail.com>
2022-10-05 14:18:44 +0800

    Improved the layout of "Add Person" form

--- ncard_app/forms.py
+++ ncard_app/forms.py
@@ -2,7 +2,8 @@ from django.forms import ModelForm
 from ncard_app import models
 from django import forms
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Row, Column
+from crispy_forms.layout import Layout, Row, Column, ButtonHolder, HTML, Submit, Button
+from crispy_forms.bootstrap import FormActions, PrependedText, Tab, TabHolder
 
 class PersonForm(ModelForm):
     class Meta:
@@ -16,38 +17,76 @@ class PersonForm(ModelForm):
         self.helper.form_tag = False
         self.helper.disable_csrf = True
         self.helper.layout = Layout(
-            Row(
-                Column('title', css_class='col-md-1 mb-0'),
-                Column('given_name', css_class='col-md-4 mb-0'),
-                Column('middle_name', css_class='col-md-3 mb-0'),
-                Column('surname', css_class='col-md-4 mb-0'),
-                css_class='row'
-            ),
-            Row('email', css_class='row'),
-            Row('email2', css_class='row'),
-            Row('phone_office', css_class='row'),
-            Row('phone_mobile', css_class='row'),
-            Row('phone_home', css_class='row'),
-            Row('cre_role', css_class='row'),
-            Row('ncard_relation', css_class='row'),
-            Row('project', css_class='row'),
-            Row('display_on_website', css_class='row'),
-            Row('profile_url', css_class='row'),
-            Row('orcid_id', css_class='row'),
-            Row('scopus_id', css_class='row'),
-            Row('wos_researcher_id', css_class='row'),
-            Row('google_scholar', css_class='row'),
-            Row('researchgate', css_class='row'),
-            Row('loop_profile', css_class='row'),
-            Row('linkedin', css_class='row'),
-            Row('twitter', css_class='row'),
-            Row('employers', css_class='row'),
-            Row('location', css_class='row'),
-            Row('organisation_primary', css_class='row'),
-            Row('organisation_other', css_class='row'),
-            Row('clinician', css_class='row'),
-            Row('notes', css_class='row'),
-            Row('research_focus', css_class='row'),
+            TabHolder(
+                Tab('Contact Details',
+                    HTML("<br>"),
+                    Row(
+                        Column('title', css_class='col-md-1 mb-0'),
+                        Column('given_name', css_class='col-md-4 mb-0'),
+                        Column('middle_name', css_class='col-md-3 mb-0'),
+                        Column('surname', css_class='col-md-4 mb-0'),
+                        css_class='row'
+                    ),
+                    Row(
+                        Column('email', css_class='row'),
+                        Column('email2', '', css_class='row'),
+                        css_class='row'
+                    ),
+                    Row(
+                        Column('phone_office', css_class='row'),
+                        Column('phone_mobile', css_class='row'),
+                        Column('phone_home', css_class='row'),
+                        css_class='row'
+                    ),
+                ),
+                
+                Tab('NCARD Info',
+                    HTML("<br>"),
+                    Row(
+                        Column('cre_role', css_class='col-md-4 mb-0'),
+                        Column('ncard_relation', css_class='col-md-4 mb-0'),
+                        Column('project', css_class='col-md-4 mb-0'),
+                        css_class='row'
+                    ),
+                    Row(
+                        Column('display_on_website', css_class='col-md-3 mb-0'),
+                        Column('profile_url', css_class='col-md-9 mb-0'),
+                        css_class='row'
+                    ),
+                ),
+
+                Tab('Individal Reseracher Details',
+                    HTML("<br>"),
+                                        Row(
+                        Column('orcid_id', css_class='col-md-3 mb-0'),
+                        Column('scopus_id', css_class='col-md-3 mb-0'),
+                        Column('wos_researcher_id', css_class='col-md-3 mb-0'),
+                        css_class='row'
+                    ),
+                    Row(
+                        Column('google_scholar', css_class='col-md-4 mb-0'),
+                        Column('researchgate', css_class='col-md-4 mb-0'),
+                        Column('loop_profile', css_class='col-md-4 mb-0'),
+                        css_class='row'
+                    ),
+                    Row(
+                        Column('linkedin', css_class='col-md-4 mb-0'),
+                        Column('twitter', css_class='col-md-3 mb-0'),
+                        Column('clinician', css_class='col-md-2 mb-0'),
+                        css_class='row'
+                    ),
+                     Row('research_focus', css_class='row'),
+                ),
+
+                Tab('Organisational Info',
+                    HTML("<br>"),
+                    Row('employers', css_class='row'),
+                    Row('location', css_class='row'),
+                    Row('organisation_primary', css_class='row'),
+                    Row('organisation_other', css_class='row'),
+                    Row('notes', css_class='row'),
+                )
+            )
         )
 
 class AwardForm(ModelForm):
