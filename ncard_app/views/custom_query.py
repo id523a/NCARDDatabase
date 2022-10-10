5c69f46c0219c62fd0adf312e2d452dd0ac77dd8
Edward Giles <edward@giles.net.au>
2022-10-10 22:02:16 +0800

    Make field selection work in custom query

--- ncard_app/views/custom_query.py
+++ ncard_app/views/custom_query.py
@@ -54,6 +54,9 @@ def get_field_type(field):
     else:
         return field_type_map.get(type(field), '')
 
+def capitalize_first(name):
+    return name[0].upper() + name[1:]
+
 def get_field_friendly_name(model, field):
     key = f'{model._meta.model_name}.{field.name}'
     if key in friendly_names:
@@ -62,17 +65,30 @@ def get_field_friendly_name(model, field):
         verbose_name = field.verbose_name
     else:
         verbose_name = field.name.replace('_', ' ')
-    return verbose_name[0].upper() + verbose_name[1:]
+    return capitalize_first(verbose_name)
 
+"""
 extra_fields = {
     models.Person: [
         {'name': 'full_name', 'label': 'Full name', 'type': 'string'},
     ],
 }
+"""
 
-def schema_entry_model(model):
+def get_schema_meta(model):
+    return {
+        'singular': capitalize_first(model._meta.verbose_name),
+        'plural': capitalize_first(model._meta.verbose_name_plural)
+    }
+
+schema_meta = {model._meta.model_name: get_schema_meta(model) for model in schema_models}
+
+schema_meta['date'] = {'hide': True, 'singular': 'Date', 'plural': 'Dates'}
+
+def get_schema_fields(model):
     # Add computed fields first
-    fields = list(extra_fields.get(model, []))
+    # fields = list(extra_fields.get(model, []))
+    fields = []
     # Create list of fields by inspecting model
     for field in model._meta.get_fields():
         field_type = get_field_type(field)
@@ -80,7 +96,20 @@ def schema_entry_model(model):
             fields.append({'name': field.name, 'label': get_field_friendly_name(model, field), 'type': field_type})
     return fields
 
-schema = {model._meta.model_name: schema_entry_model(model) for model in schema_models}
+schema_fields = {model._meta.model_name: get_schema_fields(model) for model in schema_models}
+
+schema_fields['date'] = [
+    {'name': 'year', 'label': 'Year', 'type': 'integer'},
+    {'name': 'month', 'label': 'Month', 'type': 'integer'},
+    {'name': 'day', 'label': 'Day of month', 'type': 'integer'},
+    {'name': 'week', 'label': 'Week of year', 'type': 'integer'},
+    {'name': 'week_day', 'label': 'Day of week', 'type': 'integer'}
+]
+
+schema = {
+    'meta': schema_meta,
+    'fields': schema_fields
+}
 
 @cache_control(max_age=86400)
 @api_login_required
@@ -94,9 +123,33 @@ def custom_query(request):
 @api_login_required
 def custom_query_data(request):
     if not request.method == 'POST':
-        raise SuspiciousOperation()
+        raise SuspiciousOperation("Must use POST request.")
     if len(request.body) > 16384:
-        raise SuspiciousOperation()
+        raise SuspiciousOperation("Query is too long.")
     body = json.loads(request.body)
+    
+    # Get starting model from table name
+    start_table = body["startTable"]
+    if not type(start_table) == str:
+        raise SuspiciousOperation("Invalid starting table.")
 
-    return JsonResponse(body)
+    start_model = None
+    for model in schema_models:
+        if model._meta.model_name == start_table:
+            start_model = model
+            break
+    
+    if start_model is None:
+        raise SuspiciousOperation("Invalid starting table.")
+    
+    objs = start_model.objects.all()
+    
+    # Get fields for output
+    # TODO: Validate fields
+    # TODO: Support computed fields such as full_name
+    fields = body["fields"]
+    fields_pp = ["__".join(subfields) or "id" for subfields in fields]
+    
+    # TODO: Filter the rows
+
+    return JsonResponse(list(objs.values_list(*fields_pp)), safe=False)
