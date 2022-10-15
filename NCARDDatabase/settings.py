1fa7e406f7428d7cce05bf2ab3faf5d1bf2ca342
NickkeyNikNik <nathanielong98@gmail.com>
2022-10-15 16:13:37 +0800

    Added import export into settings and pip

--- NCARDDatabase/settings.py
+++ NCARDDatabase/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
 import os
 import sys
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django_tables2',
     "django_filters",
     "bootstrap5",
-
+    'import_export',
 ]
 
 MIDDLEWARE = [
--- ncard_app/models.py
+++ ncard_app/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
+from django.core.validators import MinValueValidator, RegexValidator
 from django.db import models
-from django.core.validators import RegexValidator, MinValueValidator
 from django.urls import reverse
 
 #validators section
