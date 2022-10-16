50e589ad4766b2d42e6964a52fe2ea374003e0bc
Edward Giles <edward@giles.net.au>
2022-10-16 23:29:19 +0800

    Remove secret keys from Git repository

---
+++ NCARDDatabase/secrets/gen_secrets.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import secrets
+import os
+
+def make_secret_file(path):
+    if os.path.isfile(path):
+        print(f"{path} already exists, skipping generation")
+    else:
+        with open(path, mode='w') as f:
+            f.write(secrets.token_urlsafe(32))
+
+make_secret_file("django_secret.txt")
+make_secret_file("db_password.txt")
