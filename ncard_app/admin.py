7a5c5070d87bd6ed6cf256aa953e3b9d241cc55b
NickkeyNikNik <nathanielong98@gmail.com>
2022-10-16 10:36:20 +0800

    Edited cofiguration for grants

--- ncard_app/admin.py
+++ ncard_app/admin.py
@@ -11,7 +11,7 @@ class GrantInvestigatorInline(admin.TabularInline):
     extra = 1
 
 class GrantAdmin(ImportExportModelAdmin):
-    list_display = ("title", "reference", "project","investigators")
+    list_display = ("title", "reference", "project")
     inlines = [
         GrantInvestigatorInline
     ]
